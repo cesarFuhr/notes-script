e0179393d98d082024ddfa7d501729be78e264ca
Cesar Cara <cesar.cara@protonmail.com>
2024-02-05 15:41:50 -0300

    using flake utils

--- flake.nix
+++ flake.nix
@@ -1,25 +1,26 @@
 {
   description = "A very basic note taking script";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = self.packages.x86_64-linux.notes;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    packages.x86_64-linux.notes =
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-
+        pkgs = import nixpkgs { inherit system; };
         my-name = "notes";
+        my-buildInputs = with pkgs; [ coreutils ];
         my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./notes.sh)).overrideAttrs (old: {
           buildCommand = "${old.buildCommand}\n patchShebangs $out";
         });
-
-        my-buildInputs = with pkgs; [ coreutils ];
       in
-      pkgs.symlinkJoin {
-        name = my-name;
-        paths = [ my-script ] ++ my-buildInputs;
-        buildInputs = [ pkgs.makeWrapper ];
-        postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
-      };
-  };
+      rec {
+        defaultPackage = packages.my-script;
+        packages.my-script = pkgs.symlinkJoin {
+          name = my-name;
+          paths = [ my-script ] ++ my-buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+        };
+      }
+    );
 }
