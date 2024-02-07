f3324045f9004098d3534d2c2af2392ebb13f844
Cesar Cara <cesar.cara@protonmail.com>
2024-02-06 22:25:32 -0300

    building for several nix systems

--- flake.nix
+++ flake.nix
@@ -1,24 +1,41 @@
 {
   description = "A very basic note taking script";
+  outputs = inputs@{ self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-  outputs = inputs@{ self, nixpkgs }: {
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.notes;
-    packages.x86_64-linux.notes =
-      let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-        my-name = "notes";
-        my-buildInputs = with pkgs; [ coreutils ];
-        my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./notes.sh)).overrideAttrs (old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
-      in
-      pkgs.symlinkJoin {
-        name = my-name;
-        paths = [ my-script ] ++ my-buildInputs;
-        buildInputs = [ pkgs.makeWrapper ];
-        postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
-      };
-  };
+    in
+    {
+      packages = builtins.listToAttrs (builtins.map
+        (system: {
+          name = system;
+          value = {
+            default = self.packages.${system}.notes;
+            notes =
+              let
+                pkgs = import nixpkgs {
+                  system = system;
+                };
+                my-name = "notes";
+                my-buildInputs = with pkgs; [ coreutils ];
+                my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./notes.sh)).overrideAttrs (old: {
+                  buildCommand = "${old.buildCommand}\n patchShebangs $out";
+                });
+              in
+              pkgs.symlinkJoin {
+                name = my-name;
+                paths = [ my-script ] ++ my-buildInputs;
+                buildInputs = [ pkgs.makeWrapper ];
+                postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+              };
+          };
+        })
+        systems
+      );
+    };
 }
